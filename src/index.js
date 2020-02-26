17786c8e12033aa13f4236cba6c76c480d7e4977
ppopp20 <ppopp20@cs.uw.edu>
2020-02-26 07:40:37 -0800

    simple gender breakdown pi chart for directors on calculations page

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ const d3 = require('d3');
 		}
 		makeSlider();	
 		makeCountryCarousel();
+		makePiChart();
 	}
 
 	function makeSlider() {
@@ -88,6 +89,42 @@ const d3 = require('d3');
 		  wrapAround: true,
 		  pageDots: false
 		});
+	}
+
+	function makePiChart() {
+		var data = [177, 8]; // numbers from director_gender.txt
+
+		var svg = d3.select("#pi-chart"),
+			width = svg.attr("width"),
+			height = svg.attr("height"),
+			radius = Math.min(width, height) / 2,
+			g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+		var color = d3.scaleOrdinal(['#4974B9','#A157A2']);
+
+		// Generate the pie
+		var pie = d3.pie();
+
+		// Generate the arcs
+		var arc = d3.arc()
+					.innerRadius(0)
+					.outerRadius(radius - 4);
+
+		//Generate groups
+		var arcs = g.selectAll("arc")
+					.data(pie(data))
+					.enter()
+					.append("g")
+					.attr("class", "arc")
+        			.attr("stroke", "white")
+  					.style("stroke-width", "2px")
+
+		//Draw arc paths
+		arcs.append("path")
+			.attr("fill", function(d, i) {
+				return color(i);
+			})
+			.attr("d", arc);
 	}
 
 	function clickGenre(elements, index) {
