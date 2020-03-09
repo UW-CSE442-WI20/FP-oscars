0801d79ef3684192db5334f067f828cdd262ed40
Olga Andreeva <andreolg@cs.washington.edu>
2020-03-08 22:07:28 -0700

    Update website docs

--- docs/nouislider.0880b701.js
+++ docs/nouislider.0880b701.js
@@ -2498,7 +2498,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60253" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54781" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
